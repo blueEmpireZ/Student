f62c222c01153902706940c78ea949ce685fe7e1
kaur8656 <65624142+kaur8656@users.noreply.github.com>
2020-06-01 14:06:11 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -1,4 +1,6 @@
-/*
+/*ID: 991587501
+*Exercise 1
+*@modfier harmandeep kaur
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,13 +22,52 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+             c.setValue(c.randomValue());
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[c.randomSuit()]);
+            magicHand[i]=c;
+            System.out.println(magicHand[i].getSuit()+"  "+magicHand[i].getValue());
         }
-        
+         System.out.println("Enter card value:");
+        int num=inp.nextInt();
+        System.out.print("\nenter a  suit of a card(Hearts, Diamonds, Spades, Clubs): ");
+
+        String  suit=inp.nextLine();
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+         Scanner c = new Scanner(System.in);
+
+        System.out.print("enter a value of a card: ");
+        int v = c.nextInt();
+
+        boolean match;
+        for (Card magicHand1 : magicHand) {
+            if (num == (magicHand1.getValue()) && (suit.equals(magicHand1.getSuit()))) {
+                match=true;
+                System.out.println("your card matches in magicHand");
+            } else {
+                System.out.println("your card does not matches in magicHand");
+            }
+        }
+        Card LuckyCard=new Card();
+        LuckyCard.setValue(3);
+        LuckyCard.setSuit("spades");
+        System.out.println( LuckyCard.getValue() +"    "+ LuckyCard.getSuit());
+
+
+        boolean valid;
+        for (int i=0; i<magicHand.length;i++) {
+            if(LuckyCard.getValue()==(magicHand[i].getValue())&&
+                    LuckyCard.getSuit().equals(magicHand[i].getSuit())){
+                System.out.println("you got right");
+            }
+            else{
+                System.out.println("you lose");
+            }
+        }
+
     }
     
 }
